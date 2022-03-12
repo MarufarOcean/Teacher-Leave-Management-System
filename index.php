c79f3da79e1e96a8d552ab77a1e564b6ea00fc04
MarufarOcean <75928219+MarufarOcean@users.noreply.github.com>
2022-03-13 00:28:53 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>TEACHER LEAVE MANAGEMENT SYSTEM</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+  </head>
+  <body>
+
+    <div class="main">
+      <div class="inner">
+        <div class="opacity"></div>
+        <div class="main_content">
+          <div class="logo">
+            <img src="assets/images/Bauet.png" alt="bauet">
+          </div>
+          <div class="title">
+            <h1>BANGLADESH ARMY UNIVERSITY OF ENGINEERING AND TECHNOLOGY(BAUET)  <br> <span class="location"> QUADIRABAD CANTONMENT, NATORE</span></h1>
+          </div>
+          <div class="project">
+            <h2 class="text-center">ONLINE LEAVE MANAGEMENT SYSTEM</h2>
+          </div>
+          <div class="button">
+              <p class="text-center" > <a class="btn btn-default" href="login.php">LOGIN</a> </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="assets/js/jquery.min.js" charset="utf-8"></script>
+    <script src="assets/js/bootstrap.min.js" charset="utf-8"></script>
+  </body>
+</html>
